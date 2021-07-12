598ff6d21b059f434adc71679e9f2e047d8f4e21
Andrew Green <green.andrew.b@gmail.com>
2021-07-12 12:43:24 -0500

    staticfiles

--- eventtranspotracker/settings.py
+++ eventtranspotracker/settings.py
@@ -163,9 +163,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
